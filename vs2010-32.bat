8980d3edf0c9ca18f9ea39a810a03a070009d192
WizStriver1 <1954620922@qq.com>
2020-05-28 15:37:49 +0800

    add vs2010-32/vs2010-64

---
+++ vs2010-32.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set path=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;%path%
+
+call "vcvars32.bat"
+
+cmd 
---
+++ vs2010-64.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set path=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64;%path%
+
+call "vcvars64.bat"
+
+cmd
