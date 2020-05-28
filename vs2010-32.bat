ee82f1316467aaee0fb62c3210b0341bff97663e
WizStriver1 <1954620922@qq.com>
2020-05-28 15:59:40 +0800

    update vs2020

--- vs2010-32.bat
+++ vs2010-32.bat
@@ -3,5 +3,3 @@
 set path=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;%path%
 
 call "vcvars32.bat"
-
-cmd 
--- vs2010-64.bat
+++ vs2010-64.bat
@@ -3,5 +3,3 @@
 set path=D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64;%path%
 
 call "vcvars64.bat"
-
-cmd
